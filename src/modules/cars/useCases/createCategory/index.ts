2533fc8b726bea6697b3060b0f5dca0294d44ecf
AlexsandroFGValencio <alexsandro.valencio@cialdnb.com>
2021-03-19 09:12:23 -0300

    create use case to list categories

---
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -0,0 +1,13 @@
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+import { CreateCategoryController } from "./CreateCategoryController";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+const categoriesRepository = new CategoriesRepository();
+
+const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+
+const createCategoryControllers = new CreateCategoryController(
+  createCategoryUseCase
+);
+
+export { createCategoryControllers };
